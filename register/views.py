21ecb0fa7ca9c0535e41cac18d8e7d9a622a6f1c
AnavMidha <anavmid@gmail.com>
2026-02-05 19:30:30 +0530

    First commit for turf booking system

---
+++ register/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+def signup(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        phone_number=request.POST.get("phone_number")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirm_password=request.POST.get("confirm_password")
+
+        if password != confirm_password:
+            messages.error(request,"passwords do not match")
+            return redirect("signup")
+        
+        if User.objects.filter(username=email).exists():
+            messages.error(request,"user already exists")
+            return redirect ("signup")
+        
+        user=User.objects.create_user(
+            username=email,
+            email=email,
+            password=password,
+        )
+        user.first_name=name
+        user.save()
+
+        messages.success(request,"Account created successfully.Please login")
+        return redirect("login")
+    return render(request,"signup.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username=request.POST.get("email")
+        password=request.POST.get("password")
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Logged in successfully")   
+            return redirect("home")
+        else:
+            messages.error(request,"Invalid email or password")
+    
+    return render(request,"login.html")
+
+    
